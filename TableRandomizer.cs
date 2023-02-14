85708d1d3b26c8c5b19ab3593cee31b799d6647d
evans-tim <timjevans2000@gmail.com>
2023-02-14 14:35:27 -0700

    randomizer to place table object and slightly alter the position

---
+++ TableRandomizer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using UnityEngine.Perception.Randomization.Parameters;
+using UnityEngine.Perception.Randomization.Randomizers.Utilities;
+using UnityEngine.Perception.Randomization.Samplers;
+
+namespace UnityEngine.Perception.Randomization.Randomizers.SampleRandomizers
+{
+    /// <summary>
+    /// Creates multiple layers of evenly distributed but randomly placed objects
+    /// </summary>
+    [Serializable]
+    [AddRandomizerMenu("Perception/Table Randomizer")]
+    public class TableRandomizer : Randomizer
+    {
+        /// <summary>
+        /// The Z offset component applied to all generated background layers
+        /// </summary>
+        [Tooltip("The Z offset applied to positions of all placed objects.")]
+        public float depth;
+        
+
+        /// <summary>
+        /// A categorical parameter for sampling random prefabs to place
+        /// </summary>
+        [Tooltip("The list of Prefabs to be placed by this Randomizer.")]
+        public GameObjectParameter prefabs;
+
+        GameObject m_Container;
+        GameObjectOneWayCache m_GameObjectOneWayCache;
+        System.Random random = new System.Random();
+
+        /// <inheritdoc/>
+        protected override void OnAwake()
+        {
+            m_Container = new GameObject("BackgroundContainer");
+            m_Container.transform.parent = scenario.transform;
+            m_GameObjectOneWayCache = new GameObjectOneWayCache(
+                m_Container.transform, prefabs.categories.Select((element) => element.Item1).ToArray());
+        }
+
+        /// <summary>
+        /// Generates background layers of objects at the start of each scenario iteration
+        /// </summary>
+        protected override void OnIterationStart()
+        {
+            var instance = m_GameObjectOneWayCache.GetOrInstantiate(prefabs.Sample());
+            instance.transform.position = new Vector3(random.Next(-10, 10)/5.0f, random.Next(-20, 20)/ 10.0f, 2.00f);
+            instance.transform.rotation = Quaternion.Euler(-180, 90, -90);
+        }
+
+        /// <summary>
+        /// Deletes generated background objects after each scenario iteration is complete
+        /// </summary>
+        protected override void OnIterationEnd()
+        {
+            m_GameObjectOneWayCache.ResetAllObjects();
+        }
+    }
+}
